c50729a566657d9ea868de0664948efb63b88fa5
Mahdi <mahdineer@pm.me>
2021-08-14 01:08:49 +0430

    feat: add project config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
-export default MyApp
---
+++ src/features/Login/index.tsx
@@ -0,0 +1,53 @@
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import styles from "@/styles/Login.module.css";
+import { useLogin } from "./hooks/useLogin";
+
+export default function Login() {
+  const { formMethods, submit, error, loading } = useLogin();
+  const { register, handleSubmit } = formMethods;
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(submit)} className={styles.form}>
+        <h1 className={styles.logo}>Note</h1>
+        {error && (
+          <div className="mt-3 w-full">
+            <div className={styles.error}>{error.message}</div>
+          </div>
+        )}
+        <div className="mt-2">
+          <Input
+            type="text"
+            name="username"
+            placeholder="Username"
+            label="Username"
+            register={register("username", {
+              required: true,
+            })}
+          />
+        </div>
+        <div className="mt-4">
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            label="Password"
+            register={register("password", {
+              required: true,
+            })}
+          />
+        </div>
+        <div className="mt-5">
+          <Button
+            loading={loading}
+            type="submit"
+            className={styles.submitButton}>
+            Log In
+          </Button>
+        </div>
+      </form>
+      <p className={styles.footer}>All rights reserved @ Note 2021</p>
+    </div>
+  );
+}
