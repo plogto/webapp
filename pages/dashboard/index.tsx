2fc0393982665909458100532ea0e29136f19a52
Mahdi <mahdineer@pm.me>
2021-08-15 16:31:14 +0430

    feat: add Dashboard page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "@/features/Dashboard";
+import Head from "next/head";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard | Note</title>
+      </Head>
+      <Dashboard />
+    </>
+  );
+}
---
+++ src/features/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import styles from "./Dashboard.module.css";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+    </div>
+  );
+}
