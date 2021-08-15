2bd28e7cfc56fd1b5c5be2b659a1d992efbf49f1
Mahdi <mahdineer@pm.me>
2021-08-15 16:25:37 +0430

    feat: add NavLink component

---
+++ src/components/NavLink/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styles from "./NavLink.module.css";
+
+type Props = {
+  href: string;
+  className?: string;
+  icon?: ReactNode;
+};
+
+export default function NavLink({ icon, href }: Props) {
+  const router = useRouter();
+
+  return (
+    <Link href={href}>
+      <a
+        className={`${styles.navLink} ${
+          router.pathname === href ? styles.active : ""
+        }`}>
+        {icon}
+      </a>
+    </Link>
+  );
+}
