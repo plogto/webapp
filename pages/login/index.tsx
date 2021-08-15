8ce9244f1886a3b44a95d55759300a430313d917
Mahdi <mahdineer@pm.me>
2021-08-15 16:30:48 +0430

    perf: add title for Login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,9 @@
 import Login from "@/features/Login";
+import Head from "next/head";
 
 export default function LoginPage() {
+  <Head>
+    <title>Login | Note</title>
+  </Head>;
   return <Login />;
 }
