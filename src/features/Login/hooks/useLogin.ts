5db9a86d831c3f99addcc0c49274d7075aeb9ad6
Mahdi <mahdineer@pm.me>
2021-08-17 02:08:56 +0430

    perf: improve useLogin

--- src/features/Login/hooks/useLogin.ts
+++ src/features/Login/hooks/useLogin.ts
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
+import router from "next/router";
+import { useForm } from "react-hook-form";
 import { LOGIN } from "@/graphql/auth";
 import { useLazyQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
 import type { LoginForm } from "../@types";
+import { PageUrls } from "@/@enums/pages";
+import { useAccount } from "@/context/AccountContext";
 
 export function useLogin() {
   const formMethods = useForm<LoginForm>({ mode: "all" });
-  const [login, { error, loading }] = useLazyQuery(LOGIN);
+  const [login, { error, loading, data }] = useLazyQuery(LOGIN);
+  const { setToken, setIsAutheticated } = useAccount();
+
+  useEffect(() => {
+    if (data) {
+      const { token } = data.login.authToken;
+      localStorage.setItem("Authorization", token);
+      setToken(token);
+      setIsAutheticated(true);
+      router.push(PageUrls.DASHBOARD);
+    }
+  }, [data]);
 
   const submit = (variables: LoginForm) => {
     login({ variables });
