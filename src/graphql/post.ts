ba03eb5ec55c89f177426d953d08b42f340a34fa
Mahdi <mahdineer@pm.me>
2021-08-23 01:45:24 +0430

    perf: add pagination for getUserPostsByUsername graphql query

--- src/graphql/post.ts
+++ src/graphql/post.ts
@@ -3,16 +3,24 @@ import { gql } from "@apollo/client";
 export const GET_USER_POSTS_BY_USERNAME = gql`
   query getUserPostsByUsername($username: String!) {
     getUserPostsByUsername(username: $username) {
-      id
-      user {
+      posts {
         id
-        username
-        fullname
+        user {
+          id
+          username
+          fullname
+        }
+        content
+        status
+        createdAt
+        updatedAt
       }
-      content
-      status
-      createdAt
-      updatedAt
+      pagination {
+        totalDocs
+        totalPages
+        page
+        limit
+      }
     }
   }
 `;
