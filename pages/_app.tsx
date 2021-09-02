678b8ef1dbbac0e9ec49b1758e827c9a0f062ff9
Mahdi <mahdineer@pm.me>
2021-09-03 01:56:00 +0430

    feat: add SafeHydrate component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,25 @@ import "@/styles/globals.css";
 import AppContext from "@/context";
 import AppInit from "@/components/AppInit";
 import Head from "next/head";
+import SafeHydrate from "@/components/SafeHydrate";
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <AppContext>
-        <AppInit />
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </AppContext>
-    </ApolloProvider>
+    <SafeHydrate>
+      <ApolloProvider client={apolloClient}>
+        <AppContext>
+          <AppInit />
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </AppContext>
+      </ApolloProvider>
+    </SafeHydrate>
   );
 }
