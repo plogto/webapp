e48dd285b4aab533d39aff9ee6bd1fbc5515fde8
Mahdi <mahdineer@pm.me>
2021-09-08 22:52:05 +0430

    perf: add translation for Login feature

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./en/global.json";
+import en from "./en/translation.json";
 
 export const resources = {
   en,
