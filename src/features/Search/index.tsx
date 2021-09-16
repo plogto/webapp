ab0ed0d665a1c5ea97fa5704119d044ea2d235b9
Mahdi <mahdineer@pm.me>
2021-09-16 23:34:54 +0430

    perf: improve Search feature

--- src/features/Search/index.tsx
+++ src/features/Search/index.tsx
@@ -1,57 +1,63 @@
 import styles from "./Search.module.css";
-import SearchInput from "./components/SearchInput";
 import { useSearch } from "./hooks/useSearch";
 import Users from "./components/Users";
 import Tags from "./components/Tags";
 import Button from "@/components/Button";
 import { useTranslation } from "react-i18next";
 import { HashtagIcon, UsersIcon } from "@heroicons/react/solid";
+import Wrapper from "@/components/Wrapper";
 
 export default function Search() {
   const { formMethods, submit, result, filter, setFilter } = useSearch();
   const { register, handleSubmit, watch } = formMethods;
 
-  const { t } = useTranslation("common");
-
+  const { t } = useTranslation(["common", "search"]);
   const filters = [
     {
-      title: t("users"),
+      title: t("common:users"),
       active: filter === "users",
-      icon: <UsersIcon className="w-4 h-4 mr-1" />,
+      icon: <UsersIcon />,
       onClick: () => setFilter("users"),
     },
     {
-      title: t("tags"),
+      title: t("common:tags"),
       active: filter === "tags",
-      icon: <HashtagIcon className="w-4 h-4 mr-0.5" />,
+      icon: <HashtagIcon />,
       onClick: () => setFilter("tags"),
     },
   ];
 
   return (
-    <div className={styles.wrapper}>
+    <Wrapper>
       <form className={styles.form} onSubmit={handleSubmit(submit)}>
-        <SearchInput
-          onChange={handleSubmit(() => submit(watch()))}
-          register={register("expression")}
-        />
+        <div className={styles.header}>
+          <input
+            {...register("expression")}
+            onInput={handleSubmit(() => submit(watch()))}
+            placeholder={t("search:placeholders.search")}
+            name="expression"
+            type="text"
+            autoComplete="off"
+            className={styles.searchInput}
+          />
+          <div className={styles.filters}>
+            {filters.map(f => (
+              <Button
+                key={f.title}
+                className={`${styles.filterButton} ${
+                  f.active ? styles.active : ""
+                }`}
+                onClick={f.onClick}
+              >
+                {f.icon}
+              </Button>
+            ))}
+          </div>
+        </div>
       </form>
-      <div className={styles.filters}>
-        {filters.map(f => (
-          <Button
-            key={f.title}
-            className={`${styles.filterButton} ${
-              f.active ? styles.active : ""
-            }`}
-            onClick={f.onClick}
-          >
-            {f.icon}
-            {f.title}
-          </Button>
-        ))}
-      </div>
+
       {filter == "users" && <Users user={result?.user} />}
       {filter == "tags" && <Tags tag={result?.tag} />}
-    </div>
+    </Wrapper>
   );
 }
