afcd84b8b8e57453da9b429784f74a877434a7c6
Mahdi <mahdineer@pm.me>
2021-09-16 23:35:20 +0430

    feat: implement Wrapper component

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import styles from "./Wrapper.module.css";
+
+type Props = {
+  children?: ReactNode;
+};
+
+export default function Wrapper({ children }: Props) {
+  return <div className={styles.wrapper}>{children}</div>;
+}
