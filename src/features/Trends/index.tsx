f3c20480b55ec420b800c26d3a27f8f58901b3ca
Mahdi <mahdineer@pm.me>
2021-09-18 01:06:50 +0430

    feat: implement Trends feature for desktop and mobile view

--- src/features/Trends/index.tsx
+++ src/features/Trends/index.tsx
@@ -1,35 +1,3 @@
-import { formatCountTitle } from "@/utils/formatter";
-import { TrendingUpIcon } from "@heroicons/react/solid";
-import { useTranslation } from "react-i18next";
-import { useTrends } from "./hooks/useTrends";
-import styles from "./Trends.module.css";
-import Link from "next/link";
+import { DesktopTrends, MobileTrends } from "./containers";
 
-export default function Trends() {
-  const { tags } = useTrends();
-  const { t } = useTranslation("common");
-  return (
-    <div className={styles.card}>
-      <span className={styles.header}>
-        <span className={styles.title}>{t("trends")}</span>
-        <TrendingUpIcon className="w-5 h-5" />
-      </span>
-      {tags?.map(({ id, name, count }) => (
-        <Link key={id} href={`/t/${name}`}>
-          <a className={styles.tag}>
-            <span className={styles.tagName}>#{name}</span>
-            <span className={styles.tagCount}>
-              {
-                formatCountTitle({
-                  singular: t("post"),
-                  plural: t("posts"),
-                  count,
-                }).text
-              }
-            </span>
-          </a>
-        </Link>
-      ))}
-    </div>
-  );
-}
+export { DesktopTrends, MobileTrends };
