42a8c3131aae37ad9f1631f0952be745433d9a9a
Mahdi <mahdineer@pm.me>
2021-10-04 19:04:00 +0330

    chore: update packages

--- src/features/Search/index.tsx
+++ src/features/Search/index.tsx
@@ -56,7 +56,7 @@ export function Search() {
         </div>
       </form>
 
-      <MobileOnlyView viewClassName="w-full">
+      <MobileOnlyView className="w-full">
         <MobileTrends />
       </MobileOnlyView>
 
