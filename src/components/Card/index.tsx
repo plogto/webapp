57c2a7b9f34b3fb49071a0cc41639aec4b1f340e
Mahdi <mahdineer@pm.me>
2021-11-23 13:01:43 +0330

    perf: add loading for Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,19 @@
 import { CardProps } from "./@types";
 import styles from "./Card.module.css";
+import { Loading } from "@components/Loading";
 
 export function Card(props: CardProps) {
-  const { children, className } = props;
-  return <div className={`${className} ${styles.card}`}>{children}</div>;
+  const { loading, children, className } = props;
+  return (
+    <div className={`${className} ${styles.card}`}>
+      {loading && (
+        <div className={styles.loadingContainer}>
+          <span className="relative">
+            <Loading className={styles.loading} />
+          </span>
+        </div>
+      )}
+      {children}
+    </div>
+  );
 }
