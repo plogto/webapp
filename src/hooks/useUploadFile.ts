29ba620e379d2b82797c31bc699804f815e5e860
Mahdi <mahdineer@pm.me>
2022-03-11 00:39:48 +0300

    feat: add useUploadFile hook

---
+++ src/hooks/useUploadFile.ts
@@ -0,0 +1,10 @@
+import { useMutation } from "@apollo/client";
+import { SINGLE_UPLOAD_FILE } from "@graphql/uploadFile";
+import type { SingleUploadFileMutation } from "@graphql/@types/uploadFile";
+
+export function useUploadFile() {
+  const [singleUploadFile, { data, loading, error }] =
+    useMutation<SingleUploadFileMutation>(SINGLE_UPLOAD_FILE);
+
+  return { singleUploadFile, data, loading, error };
+}
