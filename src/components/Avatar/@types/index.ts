9e8f5879d8b096552e9c32090bfb0b74e2dd1fc2
Mahdi <mahdineer@pm.me>
2022-03-12 17:40:55 +0300

    perf: add avatar prop for Avatar component

--- src/components/Avatar/@types/index.ts
+++ src/components/Avatar/@types/index.ts
@@ -1,6 +1,8 @@
 import type { AvatarSize } from "@t/size";
+import type { User } from "@t/user";
 
 export type AvatarProps = {
+  avatar?: User["avatar"];
   size?: AvatarSize;
   className?: string;
 };
