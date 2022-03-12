fe569eccb36a171151ceb5a126fee13dbf82ca6b
Mahdi <mahdineer@pm.me>
2022-03-13 00:50:35 +0300

    perf: add layout prop for LinkButton component

--- src/components/Buttons/LinkButton/@types/index.ts
+++ src/components/Buttons/LinkButton/@types/index.ts
@@ -1,6 +1,7 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export type LinkButtonProps = {
+  layout?: "cancel" | "remove" | "apply";
   href: string;
   className?: string;
   children?: ReactNode;
