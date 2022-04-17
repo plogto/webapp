2b6838d709432fbf962442c52ef416579abf0781
Mahdi <mahdineer@pm.me>
2022-04-17 18:50:51 +0300

    perf: use PageHeader for AddPost and Settings features

--- src/features/Settings/index.tsx
+++ src/features/Settings/index.tsx
@@ -9,28 +9,17 @@ import { SETTINGS_ITEMS } from "./constants";
 import { useSettings } from "./hooks/useSettings";
 import { Card } from "@components/Card";
 import { Icon } from "@components/Icon";
+import { PageHeader } from "@components/PageHeader";
 import { ModalProvider } from "@contexts/ModalContext";
-import { PageUrls } from "@enums/pages";
-import { useNavigation } from "@hooks/useNavigation";
 
 export function Settings() {
   const { user } = useSettings();
-  const { formatProfilePageRoute } = useNavigation();
   const { t } = useTranslation("settings");
 
   return (
     <>
       <Card className={styles.settings}>
-        <div className={styles.header}>
-          <Link
-            href={user ? formatProfilePageRoute(user?.username) : PageUrls.HOME}
-          >
-            <a className={styles.back}>
-              <Icon className={styles.icon} name="chevronLeft" />
-            </a>
-          </Link>
-          <h2 className={styles.title}>{t("texts.settings")}</h2>
-        </div>
+        <PageHeader title={t("texts.settings")} />
         <div className={styles.images}>
           <ModalProvider>
             <BackgroundProfile background={user?.background} />
