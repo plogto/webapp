df57f10c0ad13961aa976d014d61b8281c32dd0e
Mahdi <mahdineer@pm.me>
2022-05-13 21:45:57 +0300

    fix: add validationSchema for login form

--- src/features/Auth/hooks/useLogin.ts
+++ src/features/Auth/hooks/useLogin.ts
@@ -1,16 +1,28 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import * as Yup from "yup";
 import { useLazyQuery } from "@apollo/client";
 import { useAccountContext } from "@contexts/AccountContext";
 import { PageUrls } from "@enums/pages";
 import type { LoginQuery } from "@graphql/@types/auth";
 import { LOGIN } from "@graphql/auth";
+import { yupResolver } from "@hookform/resolvers/yup";
 import type { LoginForm } from "../Auth.types";
 import { useRouter } from "next/router";
 
 export function useLogin() {
   const { push } = useRouter();
-  const formMethods = useForm<LoginForm>({ mode: "all" });
+  const { t } = useTranslation("auth");
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required(t("errors.usernameIsRequired")),
+    password: Yup.string().required(t("errors.passwordIsRequired")),
+  });
+
+  const formMethods = useForm<LoginForm>({
+    mode: "all",
+    resolver: yupResolver(validationSchema),
+  });
   const [login, { error, loading, data }] = useLazyQuery<LoginQuery>(LOGIN);
   const { setIsAuthenticated, setToken, setUser } = useAccountContext();
 
