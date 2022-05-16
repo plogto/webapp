85e6cbc216350e28f646f128a088c052dab08380
Mahdi <mahdineer@pm.me>
2022-05-16 22:30:50 +0300

    perf: improve FollowRequests feature

--- src/features/FollowRequests/index.tsx
+++ src/features/FollowRequests/index.tsx
@@ -1,12 +1,16 @@
+import { useTranslation } from "react-i18next";
 import { Card } from "@components/Card";
+import { PageHeader } from "@components/PageHeader";
 import { User } from "@components/User";
 import styles from "./FollowRequests.module.css";
 import { useFollowRequests } from "./hooks/useFollowRequests";
 
 export function FollowRequests() {
   const { followRequests } = useFollowRequests();
+  const { t } = useTranslation("notifications");
   return (
     <Card className={styles.card}>
+      <PageHeader title={t("texts.followRequests")} />
       {followRequests?.map(followRequest => (
         <User
           key={followRequest.id}
--- src/features/Notifications/index.tsx
+++ src/features/Notifications/index.tsx
@@ -1,12 +1,15 @@
 import { useTranslation } from "react-i18next";
 import { Card } from "@components/Card";
 import { PageHeader } from "@components/PageHeader";
+import { PageUrls } from "@enums/pages";
 import styles from "./Notifications.module.css";
 import { Notification } from "./components/Notification";
 import { useNotifications } from "./hooks/useNotifications";
+import Link from "next/link";
 
 export function Notifications() {
-  const { notifications, unreadNotificationsCount } = useNotifications();
+  const { followRequestsCount, notifications, unreadNotificationsCount } =
+    useNotifications();
   const { t } = useTranslation("notifications");
 
   return (
@@ -21,6 +24,12 @@ export function Notifications() {
           )
         }
       />
+
+      {followRequestsCount && followRequestsCount > 0 && (
+        <Link href={PageUrls.FOLLOW_REQUESTS}>
+          <a className={styles.followRequests}>{t("texts.followRequests")}</a>
+        </Link>
+      )}
 
       {notifications?.map(notification => (
         <Notification key={notification.id} notification={notification} />
