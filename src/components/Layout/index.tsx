3094c0c644ea3bc0710fe7bee20bcfbc4e80859a
Mahdi <mahdineer@pm.me>
2022-05-19 00:02:01 +0300

    refactor: improve Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,10 @@
+import { isMobile } from "react-device-detect";
+import { Navbar } from "@components/Navbar";
 import { useAccountContext } from "@contexts/AccountContext";
 import { PageUrls } from "@enums/pages";
 import { Search } from "@features/Search";
 import { DesktopTrends } from "@features/Trends";
-import { Navbar } from "../Navbar";
-import { SidebarNavigation } from "../SidebarNavigation";
+import { Header } from "../Header";
 import { LayoutProps } from "./@types";
 import styles from "./Layout.module.css";
 import { useRouter } from "next/router";
@@ -16,17 +17,21 @@ export function Layout(props: LayoutProps) {
   const limitedRoutes = [PageUrls.SEARCH];
 
   return user ? (
-    <div className={styles.layout}>
-      <Navbar />
-      <SidebarNavigation />
-      {children}
-      <div className="hidden md:block w-8/12 mt-4">
-        <div className="sticky top-4 flex flex-col space-y-4 mb-4">
-          {!limitedRoutes.includes(pathname as PageUrls) && <Search />}
-          <DesktopTrends />
+    <>
+      {!isMobile && <Header user={user} />}
+      <div className={styles.layout}>
+        {children}
+        <div className="hidden md:block w-8/12 mt-4">
+          <div className="sticky top-4 flex flex-col space-y-4 mb-4">
+            {!limitedRoutes.includes(pathname as PageUrls) && <Search />}
+            <DesktopTrends />
+          </div>
+        </div>
+        <div className={styles.footer}>
+          {isMobile && <Navbar user={user} />}
         </div>
       </div>
-    </div>
+    </>
   ) : (
     <div className="mx-auto w-full md:w-[36.5rem]">{children}</div>
   );
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,58 @@
+import { v4 as uuid } from "uuid";
+import { Avatar } from "@components/Avatar";
+import { Icon } from "@components/Icon";
+import { PageUrls } from "@enums/pages";
+import { useClassName } from "@hooks/useClassName";
+import { NavigationItem } from "@t/navigation";
+import styles from "./Navbar.module.css";
+import { NavbarProps } from "./Navbar.types";
+import Link from "next/link";
+
+export function Navbar(props: NavbarProps) {
+  const { user } = props;
+  const { activeClass } = useClassName();
+
+  const NAVIGATION_ITEMS: NavigationItem[] = [
+    {
+      icon: <Icon name="BellFill" />,
+      href: PageUrls.NOTIFICATIONS,
+    },
+    {
+      icon: <Icon name="GlobeFill" />,
+      href: PageUrls.SEARCH,
+    },
+    {
+      icon: <Icon name="PlusCircleFill" />,
+      href: PageUrls.ADD_POST,
+    },
+    {
+      icon: <Icon name="ViewGridFill" />,
+      href: PageUrls.HOME,
+    },
+    {
+      icon: (
+        <Avatar size="tiny" className={styles.avatar} avatar={user?.avatar} />
+      ),
+      href: `/${user?.username}`,
+    },
+  ];
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.navbar}>
+        {NAVIGATION_ITEMS.map(({ href, icon }) => (
+          <Link key={uuid()} href={href}>
+            <a
+              className={`${styles.navLink} ${activeClass({
+                href,
+                className: styles.active,
+              })}`}
+            >
+              {icon}
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
