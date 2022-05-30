9c204021ccd79a513625f8083d54769440d18839
Mahdi <mahdineer@pm.me>
2022-05-30 10:15:43 +0300

    perf: improve Posts component

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,17 +1,27 @@
 import InfiniteScroll from "react-infinite-scroll-component";
 import classNames from "classnames";
 import { POST_TYPES } from "@constants";
+import { ContentStatus } from "@components/ContentStatus";
 import { Loader } from "@components/Loader";
 import { Post } from "@components/Post";
 import styles from "./Posts.module.css";
 import type { PostsProps } from "./Posts.types";
 
 export function Posts(props: PostsProps) {
-  const { posts, className, pagination, getMoreData } = props;
+  const {
+    posts,
+    scrollableTarget,
+    className,
+    pagination,
+    getMoreData,
+    emptyStatus: { title, description, icon },
+  } = props;
   const wrapperClasses = classNames(styles.posts, className);
 
   return (
     <InfiniteScroll
+      scrollableTarget={scrollableTarget}
+      className={wrapperClasses}
       dataLength={posts?.length || 0}
       next={getMoreData}
       hasMore={!!pagination?.nextPage}
@@ -20,16 +30,24 @@ export function Posts(props: PostsProps) {
           <Loader className={styles.loading} />
         </div>
       }
-      className={wrapperClasses}
     >
-      {posts?.map(post => (
-        <Post
-          key={post.id}
-          post={post}
-          className={styles.post}
-          type={POST_TYPES.CARD}
+      {!posts || posts?.length < 1 ? (
+        <ContentStatus
+          title={title}
+          description={description}
+          icon={icon}
+          className={styles.emptyStatus}
         />
-      ))}
+      ) : (
+        posts?.map(post => (
+          <Post
+            key={post.id}
+            post={post}
+            className={styles.post}
+            type={POST_TYPES.CARD}
+          />
+        ))
+      )}
     </InfiniteScroll>
   );
 }
--- src/components/Posts/Posts.types.ts
+++ src/components/Posts/Posts.types.ts
@@ -1,10 +1,13 @@
 import type { Pagination } from "@t/pagination";
 import type { Post } from "@t/post";
+import { Status } from "@t/status";
 
 export interface PostsProps {
   posts?: Post[];
+  scrollableTarget: string;
   className?: string;
   isLoading?: boolean;
   pagination?: Pagination;
   getMoreData: () => void;
+  emptyStatus: Status;
 }
