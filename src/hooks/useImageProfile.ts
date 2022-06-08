aa8c5589a442f583ce6f8eee39c5910ab05d415a
Mahdi <mahdineer@pm.me>
2022-06-09 00:45:57 +0300

    chore: move useImageProfile to hooks

---
+++ src/hooks/useImageProfile.ts
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useMutation } from "@apollo/client";
+import { useModalContext } from "@contexts/ModalContext";
+import type { EditUserMutation } from "@graphql/@types/user";
+import { EDIT_USER } from "@graphql/user";
+import { useUploadFile } from "@hooks/useUploadFile";
+import type { UseImageProfileProps } from "./@types";
+
+export function useImageProfile(props: UseImageProfileProps) {
+  const { key } = props;
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [imagePreview, setImagePreview] = useState<Blob>();
+  const { closeModal, openModal, isOpen } = useModalContext();
+  const [editUser] = useMutation<EditUserMutation>(EDIT_USER);
+  const {
+    singleUploadFile,
+    singleUploadFileResponse: { data },
+  } = useUploadFile();
+
+  useEffect(() => {
+    if (data?.singleUploadFile) {
+      editUser({ variables: { [key]: data.singleUploadFile.id } });
+    }
+  }, [data, editUser, key]);
+
+  useEffect(() => {
+    if (imagePreview) {
+      const file = new File([imagePreview], "file.png", {
+        type: "image/png",
+      });
+      singleUploadFile({
+        variables: { file },
+      });
+    }
+  }, [imagePreview, singleUploadFile]);
+
+  const removeImage = useCallback(() => {
+    editUser({ variables: { [key]: "" } });
+    closeModal();
+  }, [closeModal, editUser, key]);
+
+  const onClickInputFile = useCallback(() => {
+    closeModal();
+    inputFileRef.current?.click();
+  }, [closeModal]);
+
+  return {
+    inputFileRef,
+    onClickInputFile,
+    setImagePreview,
+    closeModal,
+    openModal,
+    isOpen,
+    removeImage,
+  };
+}
