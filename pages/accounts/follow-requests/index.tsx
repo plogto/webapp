ec4558a6a77bf99e7e5be0cc2355dc617a462b43
Mahdi <mahdineer@pm.me>
2022-06-22 23:31:25 +0300

    perf: improve FollowRequests feature

--- pages/accounts/follow-requests/index.tsx
+++ pages/accounts/follow-requests/index.tsx
@@ -1,9 +1,7 @@
 import { useTranslation } from "react-i18next";
-import { Layout } from "@components/Layout";
-import { Wrapper } from "@components/Wrapper";
-import { FollowRequestsProvider } from "@contexts/FollowRequestsContext";
-import { FollowRequests } from "@features/FollowRequests";
 import Head from "next/head";
+import { Layout } from "@components/Layout";
+import { FollowRequests } from "@features/FollowRequests";
 
 export default function FollowRequestsPage() {
   const { t } = useTranslation(["common", "pages"]);
@@ -14,13 +12,9 @@ export default function FollowRequestsPage() {
           {t("pages:followRequests")} | {t("common:logoName")}
         </title>
       </Head>
-      <FollowRequestsProvider>
-        <Layout>
-          <Wrapper>
-            <FollowRequests />
-          </Wrapper>
-        </Layout>
-      </FollowRequestsProvider>
+      <Layout showTrends>
+        <FollowRequests />
+      </Layout>
     </>
   );
 }
