823e734131ad799ac7204f580d22d909d9b9282e
Mahdi <mahdineer@pm.me>
2022-07-05 00:32:43 +0300

    perf: remove limitedRoutes from Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,6 @@
 import { isMobile } from "react-device-detect";
-import { useRouter } from "next/router";
 import { Navbar } from "@components/Navbar";
 import { useAccountContext } from "@contexts/AccountContext";
-import { PageUrls } from "@enums/pages";
-import { Search } from "@features/Search";
 import { DesktopTrends } from "@features/Trends";
 import { Header } from "../Header";
 import styles from "./Layout.module.css";
@@ -12,31 +9,29 @@ import type { LayoutProps } from "./Layout.types";
 export function Layout(props: LayoutProps) {
   const { children, showTrends } = props;
   const { user } = useAccountContext();
-  const { pathname } = useRouter();
 
-  const limitedRoutes: string[] = [];
-
-  return user ? (
+  return (
     <>
       <Header user={user} />
-      <div className={styles.layout}>
-        <div className="flex items-start md:space-x-4 w-full md:mx-2.5">
-          {children}
-          {showTrends && (
-            <div className="hidden md:block w-8/12 mt-4">
-              <div className="sticky top-[4.5rem] flex flex-col space-y-4 mb-4">
-                {limitedRoutes?.includes(pathname as PageUrls) && <Search />}
-                <DesktopTrends />
+      {user ? (
+        <div className={styles.layout}>
+          <div className="flex items-start md:space-x-4 w-full md:mx-2.5">
+            {children}
+            {showTrends && (
+              <div className="hidden md:block w-8/12 mt-4">
+                <div className="sticky top-[4.5rem] flex flex-col space-y-4 mb-4">
+                  <DesktopTrends />
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
+          <div className={styles.footer}>
+            {isMobile && <Navbar user={user} />}
+          </div>
         </div>
-        <div className={styles.footer}>
-          {isMobile && <Navbar user={user} />}
-        </div>
-      </div>
+      ) : (
+        <div className={styles.layout}>{children}</div>
+      )}
     </>
-  ) : (
-    <div className={styles.layout}>{children}</div>
   );
 }
