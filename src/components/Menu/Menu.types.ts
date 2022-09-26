f01c8b0b19ffbd4e98711ed049f65b75f9c40360
Mahdi <mahdineer@pm.me>
2022-09-26 23:44:52 +0200

    refactor(Menu): improve styles and types

--- src/components/Menu/Menu.types.ts
+++ src/components/Menu/Menu.types.ts
@@ -1,16 +1,16 @@
 import type { ReactNode } from "react";
 import type { IconNames } from "@components/Icon";
 
-interface MenuItem {
-  key: string;
-  type?: "normal" | "delete";
+interface MenuItem<T> {
+  key: T;
+  type?: "normal" | "delete" | "success" | "info";
   title: string;
   icon: IconNames;
   onClick?: () => void;
 }
 
-export interface MenuProps {
-  items: MenuItem[];
+export interface MenuProps<T = string> {
+  items: MenuItem<T>[];
   buttonIcon: ReactNode;
   className?: string;
   itemsClassName?: string;
