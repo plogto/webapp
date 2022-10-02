4c38178687a88b11b459909c7056258087049b40
Mahdi <mahdineer@pm.me>
2022-10-02 21:08:14 +0200

    fix(Home): logout unauthenticated user

--- src/features/Home/useHome.ts
+++ src/features/Home/useHome.ts
@@ -1,27 +1,41 @@
 import { useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
 import { useLazyQuery } from "@apollo/client";
 import { useAccountContext } from "@contexts/AccountContext";
+import { PageUrls } from "@enums/pages";
 import type {
   GetTimelinePostsQuery,
   GetTimelinePostsQueryRequest,
 } from "@graphql/@types/post";
 import { GET_TIMELINE_POSTS } from "@graphql/post";
+import { useAuth } from "@hooks/useAuth";
 import type { Placeholder } from "@t/placeholder";
 
 export function useHome() {
-  const { user } = useAccountContext();
+  const { user, token } = useAccountContext();
   const [getTimelinePosts, { loading, data, fetchMore }] = useLazyQuery<
     GetTimelinePostsQuery,
     GetTimelinePostsQueryRequest
   >(GET_TIMELINE_POSTS);
   const { t } = useTranslation("post");
 
+  const { handleToken } = useAuth();
+  const { push } = useRouter();
+
   useEffect(() => {
     if (user) {
       getTimelinePosts();
     }
   }, [user, getTimelinePosts]);
+
+  useEffect(() => {
+    if (token) {
+      handleToken(token);
+    } else {
+      push(PageUrls.LOGOUT);
+    }
+  }, [token]);
 
   const emptyStatus: Placeholder = useMemo(
     () => ({
