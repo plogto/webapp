58053a071b1bdcaa2b87820aef2f2c68315bcd08
Mahdi <mahdineer@pm.me>
2022-10-15 23:49:37 +0200

    chore: update packages and improve next.config.js

--- next.config.js
+++ next.config.js
@@ -2,27 +2,29 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+const withPlugins = require("next-compose-plugins");
+
+const plugins = [withBundleAnalyzer, withPWA];
+
+const nextConfiguration = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  swcMinify: true,
+  images: {
+    domains: process.env.NEXT_PUBLIC_IMAGES_DOMAINS.split(", "),
+  },
+};
 
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer(
-  withPWA({
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-    },
-    reactStrictMode: true,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-
-      return config;
-    },
-    swcMinify: true,
-    images: {
-      domains: process.env.NEXT_PUBLIC_IMAGES_DOMAINS.split(", "),
-    },
-  }),
-);
+module.exports = withPlugins([...plugins], nextConfiguration);
--- src/components/CropImage/hooks/useCropImage.ts
+++ src/components/CropImage/hooks/useCropImage.ts
@@ -24,7 +24,7 @@ export function useCropImage(props: UseCropImageProps) {
   const [zoom, setZoom] = useState(1);
   const [pixelCrop, setPixelCrop] = useState<PixelCrop>();
 
-  const onCropComplete = useCallback((_, pixelCrop) => {
+  const onCropComplete = useCallback((_: unknown, pixelCrop: PixelCrop) => {
     setPixelCrop(pixelCrop);
   }, []);
 
