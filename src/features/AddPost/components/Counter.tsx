4caf8a69913c5ffb428e66db10a99ac147e7ab99
Mahdi <mahdineer@pm.me>
2022-10-31 11:41:27 +0100

    refactor(Counter): improve classNames

--- src/features/AddPost/components/Counter.tsx
+++ src/features/AddPost/components/Counter.tsx
@@ -1,20 +1,23 @@
+import classNames from "classnames";
 import styles from "../AddPost.module.css";
 import type { CounterProps } from "../AddPost.types";
 
 export function Counter({ length, maxLength }: CounterProps) {
+  const isCounterOverflow = length > maxLength;
+
+  const counterClasses = classNames(
+    styles.counter,
+    isCounterOverflow && styles.counterOverflow,
+  );
+
+  const numberClasses = classNames(
+    styles.number,
+    isCounterOverflow && styles.counterOverflow,
+  );
+
   return (
-    <span
-      className={`${styles.counter} ${
-        length > maxLength ? styles.counterOverflow : ""
-      }`}
-    >
-      <span
-        className={`${styles.number} ${
-          length > maxLength ? styles.counterOverflow : ""
-        }`}
-      >
-        {length}
-      </span>
+    <span className={counterClasses}>
+      <span className={numberClasses}>{length}</span>
       <span className={styles.number}>{maxLength}</span>
     </span>
   );
