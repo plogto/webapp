1451e046892c2587d845dece942b667f358229ad
Mahdi <mahdineer@pm.me>
2022-12-08 17:04:24 +0100

    refactor(Menu): implement transitions

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,6 @@
-import { Menu as HeadlessMenu } from "@headlessui/react";
+import { Menu as HeadlessMenu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 import classNames from "classnames";
-import { v4 as uuid } from "uuid";
 import { Icon } from "@components/Icon";
 import styles from "./Menu.module.css";
 import type { MenuProps } from "./Menu.types";
@@ -12,22 +12,37 @@ export function Menu(props: MenuProps) {
 
   return (
     <HeadlessMenu>
-      <div className={className}>
-        <HeadlessMenu.Items className={itemsClasses}>
-          {items.map(({ title, icon, onClick, type = "normal" }) => (
-            <HeadlessMenu.Item key={uuid()}>
-              <button
-                onClick={onClick}
-                className={classNames(styles.menuItem, styles[type])}
-              >
-                <span className={styles.title}>{title}</span>
-                {icon && <Icon name={icon} className={styles.icon} />}
-              </button>
-            </HeadlessMenu.Item>
-          ))}
-        </HeadlessMenu.Items>
-        <HeadlessMenu.Button>{buttonIcon}</HeadlessMenu.Button>
-      </div>
+      {({ open }) => {
+        return (
+          <div className={className}>
+            <Transition
+              as={Fragment}
+              show={open}
+              enter={styles.enterMenuTransition}
+              enterFrom={styles.enterFromMenuTransition}
+              enterTo={styles.enterToMenuTransition}
+              leave={styles.leaveMenuTransition}
+              leaveFrom={styles.leaveFromMenuTransition}
+              leaveTo={styles.leaveToMenuTransition}
+            >
+              <HeadlessMenu.Items className={itemsClasses} static>
+                {items.map(({ title, icon, onClick, type = "normal" }) => (
+                  <HeadlessMenu.Item key={title}>
+                    <button
+                      onClick={onClick}
+                      className={classNames(styles.menuItem, styles[type])}
+                    >
+                      <span className={styles.title}>{title}</span>
+                      {icon && <Icon name={icon} className={styles.icon} />}
+                    </button>
+                  </HeadlessMenu.Item>
+                ))}
+              </HeadlessMenu.Items>
+            </Transition>
+            <HeadlessMenu.Button>{buttonIcon}</HeadlessMenu.Button>
+          </div>
+        );
+      }}
     </HeadlessMenu>
   );
 }
