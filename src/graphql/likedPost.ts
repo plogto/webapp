2641dc117e65837d08a96f1948730129009a0684
Mahdi <mahdineer@pm.me>
2022-12-11 23:30:12 +0100

    feat: add GET_LIKED_POSTS_BY_POST_ID graphql query

--- src/graphql/likedPost.ts
+++ src/graphql/likedPost.ts
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { UserFragment } from "./fragments/user";
 
 export const LIKE_POST = gql`
   mutation likePost($postId: ID!) {
@@ -12,6 +13,26 @@ export const LIKE_POST = gql`
         likes {
           totalCount
         }
+      }
+    }
+  }
+`;
+
+export const GET_LIKED_POSTS_BY_POST_ID = gql`
+  ${UserFragment.short}
+  query getLikedPostsByPostId($postId: ID!) {
+    getLikedPostsByPostId(postId: $postId) {
+      totalCount
+      edges {
+        cursor
+        node {
+          user {
+            ...UserFragmentShort
+          }
+        }
+      }
+      pageInfo {
+        ...PageInfoFragmentComplete
       }
     }
   }
