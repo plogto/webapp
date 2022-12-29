9e5da0147e57a5a52e03dd363c77a4cb484921f1
Mahdi <mahdineer@pm.me>
2022-12-29 19:14:24 +0100

    refactor(FollowRequests): add placeholder

--- src/features/FollowRequests/FollowRequests.tsx
+++ src/features/FollowRequests/FollowRequests.tsx
@@ -2,12 +2,14 @@ import { isMobile } from "react-device-detect";
 import { useTranslation } from "react-i18next";
 import { Card } from "@components/Card";
 import { PageHeader } from "@components/PageHeader";
+import { Placeholder } from "@components/Placeholder";
 import { User } from "@components/User";
 import styles from "./FollowRequests.module.css";
 import { useFollowRequests } from "./useFollowRequests";
+import isEmpty from "lodash/isEmpty";
 
 export function FollowRequests() {
-  const { followRequests } = useFollowRequests();
+  const { followRequests, isLoading, placeholder } = useFollowRequests();
   const { t } = useTranslation("notifications");
 
   return (
@@ -26,10 +28,15 @@ export function FollowRequests() {
         className={styles.followRequests}
         shadow={!isMobile}
         rounded={!isMobile}
+        isLoading={isLoading}
       >
-        {followRequests?.edges?.map(({ node }) => (
-          <User key={node.id} user={node.follower} showAccept showDelete />
-        ))}
+        {isEmpty(followRequests?.edges) ? (
+          <Placeholder {...placeholder} className={styles.placeholder} />
+        ) : (
+          followRequests?.edges?.map(({ node }) => (
+            <User key={node.id} user={node.follower} showAccept showDelete />
+          ))
+        )}
       </Card>
     </div>
   );
--- src/features/FollowRequests/useFollowRequests.ts
+++ src/features/FollowRequests/useFollowRequests.ts
@@ -1,11 +1,16 @@
 import { useEffect, useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { isDataLoading } from "@utils";
 import { useLazyQuery } from "@apollo/client";
 import type { GetFollowRequestsQuery } from "@graphql/@types/connection";
 import { GET_FOLLOW_REQUESTS } from "@graphql/connection";
+import { Placeholder } from "@t/placeholder";
 
 export function useFollowRequests() {
-  const [getFollowRequests, { data }] =
+  const [getFollowRequests, { called, loading, data }] =
     useLazyQuery<GetFollowRequestsQuery>(GET_FOLLOW_REQUESTS);
+
+  const { t } = useTranslation("followRequests");
 
   useEffect(() => {
     getFollowRequests();
@@ -16,5 +21,18 @@ export function useFollowRequests() {
     [data?.getFollowRequests],
   );
 
-  return { followRequests };
+  const isLoading = useMemo(
+    () => isDataLoading(called, loading),
+    [called, loading],
+  );
+
+  const placeholder: Placeholder = useMemo(
+    () => ({
+      title: t("placeholders.noRequests.title"),
+      icon: "Users",
+    }),
+    [t],
+  );
+
+  return { followRequests, isLoading, placeholder };
 }
